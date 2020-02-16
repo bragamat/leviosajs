80c48657f67a0af7e006a7ee34939d3c1595e6c3
Mateus Braga <mateuscbraga@gmail.com>
2020-02-16 18:24:50 -0300

    --amend

--- startApp.js
+++ startApp.js
@@ -1,7 +1,11 @@
-const { exec, execSync } = require("child_process");
+#! /usr/bin/env node --use-strict
+
+const { execSync } = require("child_process");
 
 function startApp() {
-  execSync(`npm start`);
+  console.log();
+  // console.log(execSync(`cd ${process.cwd()}`));
+  execSync(`cd ${process.cwd()} && npm start`);
 }
 
 startApp();
