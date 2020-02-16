f22d0e53bb28c8aaaee6a400b6abcbd298114625
Mateus Braga <mateuscbraga@gmail.com>
2020-02-16 19:16:48 -0300

    --amend

--- blueprints/packageJson.js
+++ blueprints/packageJson.js
@@ -13,12 +13,12 @@ const packagejson = appName => `{
   "devDependencies": {
     "webpack": "^4.41.6",
     "webpack-cli": "^3.3.11",
-    "webpack-dev-server": "^3.10.3"
+    "webpack-dev-server": "^3.10.3",
     "babel-loader": "^8.0.6",
     "@babel/core": "^7.8.4",
     "@babel/preset-env": "^7.8.4",
     "@babel/preset-react": "^7.8.3",
-    "babel-preset-es2015": "^6.24.1",
+    "babel-preset-es2015": "^6.24.1"
   },
   "repository": {
     "type": "git",
