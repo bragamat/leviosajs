853c8f44c96cb10c3686e7eea90b7ef7705b2e6c
Mateus Braga <mateuscbraga@gmail.com>
2020-02-16 18:32:39 -0300

    --amend

--- bundleReactApp.js
+++ bundleReactApp.js
@@ -1,12 +1,12 @@
 #! /usr/bin/env node --use-strict
-const { execSync } = require("child_process");
+const { exec } = require("child_process");
 const bundleReactApp = require("./bundlers/bundleApp");
 
 const run = async () => {
   if (!process.argv[2]) return console.log("Name is needed");
 
   const app = await bundleReactApp(process.argv[2]);
-  execSync(`cd ${process.cwd()} && npm i`);
+  exec(`cd ${process.cwd()} && npm i`);
 };
 
 run();
