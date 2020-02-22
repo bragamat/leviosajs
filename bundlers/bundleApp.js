aa79b67f8c362acd720d4253e1f4bc5433e84357
Mateus Braga <mateuscbraga@gmail.com>
2020-02-22 18:43:46 -0300

    Adjusting install package.json

--- bundlers/bundleApp.js
+++ bundlers/bundleApp.js
@@ -1,17 +1,10 @@
 const mkdirp = require("mkdirp");
-const { execSync } = require("child_process");
 const bundleApp = require("./createReactApp");
 
 function bundleFolderApp(appName) {
-  mkdirp(appName)
-    .then(() => {
-      bundleApp(appName);
-    })
-    .then(() => {
-      execSync(`cd ${process.argv[2]}; npm i --verbose`, {
-        stdio: [0, 1, 2]
-      });
-    });
+  mkdirp(appName).then(() => {
+    bundleApp(appName);
+  });
 }
 
 module.exports = bundleFolderApp;
