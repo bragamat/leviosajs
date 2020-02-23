6e7efed832fc5ea1292988cefc5742f844e648b9
Mateus Braga <mateuscbraga@gmail.com>
2020-02-23 14:40:36 -0300

    removed duplicated globalstyles from src/index.js

--- blueprints/ReactComponents/reactIndex.js
+++ blueprints/ReactComponents/reactIndex.js
@@ -1,8 +1,7 @@
 const reactIndex = () => `import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './src/App'
-import GlobalStyle from './globalStyle'
 
-ReactDOM.render(<><GlobalStyle /><App /></>, document.querySelector("[react-root='app']"))`;
+ReactDOM.render(<App />, document.querySelector("[react-root='app']"))`;
 
 module.exports = reactIndex;
