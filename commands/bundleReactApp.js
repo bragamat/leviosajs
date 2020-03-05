5325d85859f923b6ca4bf3a5f9da940074bec754
Mateus Braga <mateuscbraga@gmail.com>
2020-03-04 21:15:45 -0300

    added a folder for commands

--- commands/bundleReactApp.js
+++ commands/bundleReactApp.js
@@ -1,10 +1,10 @@
 #! /usr/bin/env node
-const bundleReactApp = require("./bundlers/bundleApp");
+const bundleReactApp = require("../bundlers/bundleApp");
 
 const run = async () => {
   if (!process.argv[2]) return console.log("Name is needed");
 
-  const app = await bundleReactApp(process.argv[2]);
+  return bundleReactApp(process.argv[2]);
 };
 
 run();
