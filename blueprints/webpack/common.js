72a3233d1d9c65d87cd911ed5609dbb312e18e50
Mateus Braga <mateuscbraga@gmail.com>
2020-08-04 09:48:52 -0300

    adjusted production bundle

--- blueprints/webpack/common.js
+++ blueprints/webpack/common.js
@@ -1,33 +1,38 @@
 const webpackCommonConfig = () => `const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+
 module.exports = {
-  entry: ["react-hot-loader/patch", "@babel/polyfill", "./index.js"],
+  entry: ["./index.js"],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
   },
-  devServer: {
-    historyApiFallback: true,
-    inline: true,
-    contentBase: "./public",
-    port: 3333
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  resolve: {
+    alias: {
+      "react-dom$": "react-dom/profiling",
+      "scheduler/tracing": "scheduler/tracing-profiling"
+    }
   },
   module: {
     rules: [
       {
-        test: /\\.(js|jsx)$/,
+        test: /\.svg$/,
+        exclude: /node_modules/,
+        loader: "svg-inline-loader"
+      },
+      {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: "babel-loader",
         query: {
           presets: ["@babel/preset-env", "@babel/preset-react"]
         }
-      },
-      {
-        test: /\\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loaders: ["react-hot-loader/webpack", "babel-loader"],
-        include: path.join(__dirname, "src")
       }
     ]
   },
@@ -37,6 +42,6 @@ module.exports = {
     })
   ]
 };
-`;
+`
 
-module.exports = webpackCommonConfig;
+module.exports = webpackCommonConfig
--- blueprints/webpack/prod.js
+++ blueprints/webpack/prod.js
@@ -1,19 +1,22 @@
 const webpackProdConfig = () => `const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const webpack = require("webpack")
+
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 module.exports = merge(common, {
   mode: "production",
   optimization: {
     minimize: true
   },
   plugins: [
-    new webpack.DefinePlugin({ // <-- key to reducing React's size
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
-    new webpack.optimize.AggressiveMergingPlugin()
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new MinifyPlugin()
   ]
-});`
+})`
 
 module.exports = webpackProdConfig
